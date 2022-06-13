bd6fe19cf34ad0b00de33a14bd990cff5f73a887
devinbayly <baylyd@email.arizona.edu>
2022-06-13 14:06:13 -0700

    tracking error

--- paralleler.py
+++ paralleler.py
@@ -8,17 +8,18 @@ import os
 ncpus = os.environ.get("SLURM_NTASKS")
 print(ncpus)
 def process_pcd(pcd):
+  print(pcd)
   if not os.path.exists(pcd + "/all_scans.bin"):
     sp.run(f"./main '{pcd}'",shell=True)
 
 pcds = os.listdir("../finished")
-
+print(pcds)
 
 with mp.Pool(int(ncpus)) as pool:
   pool.map(process_pcd,["../finished/"+pcd for pcd in pcds])
 
 
-all_scans = Path("../finished/").rglob("all_scans")
-with zf.ZipFile("all_scans.zip","w") as ophile:
-    for scan in all_scans:
-        ophile.write(scan)
+#all_scans = Path("../finished/").rglob("all_scans")
+#with zf.ZipFile("all_scans.zip","w") as ophile:
+#    for scan in all_scans:
+#        ophile.write(scan)
